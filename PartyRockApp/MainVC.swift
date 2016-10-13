b190a14103d2e12a02d7307edf4bc042ed23cbc7
Jawad Shuaib <jawadshuaib@Jawads-MacBook-Pro.local>
2016-10-13 18:48:56 -0400

    UI Complete for PartyRockAnthem app

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Jawad Shuaib on 2016-10-13.
+//  Copyright © 2016 Jawad Shuaib. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+
+
+}
+
